f0f97f7185da52383b4e9ddac3ed6e159e04089d
Adriel Puello <adrielpuello@gmail.com>
2022-08-30 10:27:33 -0400

    commit tree 58c64dadf5bc746982d9bd05e57c5715b88766a0

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield')
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo')
+}
+const copyOfCats1 = ['Milo','Otis','Garfield','Broom']
+function appendCat(){
+return copyOfCats1
+}
+const copyOfCats2 = ['Arnold','Milo','Otis','Garfield']
+function prependCat(){
+    return copyOfCats2
+}
+const copyOfCats3 = ['Milo','Otis']
+function removeLastCat(){
+    return copyOfCats3
+}
+const copyOfCats4 = ['Otis','Garfield']
+function removeFirstCat(){
+    return copyOfCats4
+}
